c00dd3bc738c7055912f927bbe3941a5c2bc0400
OficeRCN <143032637+OficeRCN@users.noreply.github.com>
2024-04-01 20:23:08 -0500

    addd

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import emailRouter from "./routes/email.routes.js";
 const app = express();
 
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors({
+  origin:{
+    
+  }
+}));
 app.use(express.json());
 app.use(emailRouter);
 
